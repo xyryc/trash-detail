06663e1f528890febe8a4fed72b222e415731177
xyryc <amdanikzz@gmail.com>
2025-08-11 06:39:33 +0600

    employee: implemented employee profile UI

---
+++ app/(customer)/profile.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const CustomerProfile = () => {
+  return (
+    <View>
+      <Text>CustomerProfile</Text>
+    </View>
+  );
+};
+
+export default CustomerProfile;
--- app/(employee)/profile.tsx
+++ app/(employee)/profile.tsx
@@ -1,12 +1,215 @@
+import ButtonSecondary from "@/components/shared/ButtonSecondary";
+import CustomHeader from "@/components/shared/CustomHeader";
+import { Octicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { SafeAreaView, ScrollView, StatusBar, Text, View } from "react-native";
 
-const Profile = () => {
+const EmployeeProfile = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+      <StatusBar barStyle="dark-content" backgroundColor="white" />
+
+      <View className="flex-1 px-6">
+        {/* header */}
+        <CustomHeader text="Profile" />
+
+        {/* main content */}
+        <ScrollView
+          className="py-4"
+          contentContainerClassName="pb-10"
+          showsVerticalScrollIndicator={false}
+        >
+          <View className="border border-neutral-light-hover p-4 rounded-lg mt-6">
+            {/* first row */}
+            <View>
+              <Text
+                style={{ fontFamily: "SourceSans3-Regular" }}
+                className="text-neutral-normal mb-2"
+              >
+                ID:
+              </Text>
+              <Text
+                style={{ fontFamily: "SourceSans3-SemiBold" }}
+                className="text-neutral-dark-active"
+              >
+                E45
+              </Text>
+            </View>
+
+            {/* divider */}
+            <View className="h-px bg-neutral-light-hover my-5" />
+
+            {/* second row */}
+            <View>
+              <Text
+                style={{ fontFamily: "SourceSans3-Regular" }}
+                className="text-neutral-normal mb-2"
+              >
+                Name
+              </Text>
+              <Text
+                style={{ fontFamily: "SourceSans3-SemiBold" }}
+                className="text-neutral-dark-active"
+              >
+                Anik
+              </Text>
+            </View>
+
+            {/* divider */}
+            <View className="h-px bg-neutral-light-hover my-5" />
+
+            {/* third row */}
+            <View>
+              <Text
+                style={{ fontFamily: "SourceSans3-Regular" }}
+                className="text-neutral-normal mb-2"
+              >
+                Email
+              </Text>
+              <Text
+                style={{ fontFamily: "SourceSans3-SemiBold" }}
+                className="text-neutral-dark-active"
+              >
+                mdtalathunnabi@gmail.com
+              </Text>
+            </View>
+
+            {/* divider */}
+            <View className="h-px bg-neutral-light-hover my-5" />
+
+            {/* fourth row */}
+            <View>
+              <Text
+                style={{ fontFamily: "SourceSans3-Regular" }}
+                className="text-neutral-normal mb-2"
+              >
+                Number
+              </Text>
+              <Text
+                style={{ fontFamily: "SourceSans3-SemiBold" }}
+                className="text-neutral-dark-active"
+              >
+                +8801570233979
+              </Text>
+            </View>
+
+            {/* divider */}
+            <View className="h-px bg-neutral-light-hover my-5" />
+
+            {/* fifth row */}
+            <View>
+              <Text
+                style={{ fontFamily: "SourceSans3-Regular" }}
+                className="text-neutral-normal mb-2"
+              >
+                Address Line 1
+              </Text>
+              <Text
+                style={{ fontFamily: "SourceSans3-SemiBold" }}
+                className="text-neutral-dark-active"
+              >
+                5th Avenue
+              </Text>
+            </View>
+
+            {/* divider */}
+            <View className="h-px bg-neutral-light-hover my-5" />
+
+            {/* sixth row */}
+            <View>
+              <View className="flex-row gap-1">
+                <Text
+                  style={{ fontFamily: "SourceSans3-Regular" }}
+                  className="text-neutral-normal mb-2"
+                >
+                  Address Line 2
+                </Text>
+
+                <Text
+                  style={{ fontFamily: "SourceSans3-Regular" }}
+                  className="text-neutral-normal mb-2"
+                >
+                  (optional)
+                </Text>
+              </View>
+              <Text
+                style={{ fontFamily: "SourceSans3-SemiBold" }}
+                className="text-neutral-dark-active"
+              >
+                Bay City Avenue
+              </Text>
+            </View>
+
+            {/* divider */}
+            <View className="h-px bg-neutral-light-hover my-5" />
+
+            {/* seventh row */}
+            <View className="mb-5 flex-row justify-between pr-6">
+              {/* city */}
+              <View>
+                <Text
+                  style={{ fontFamily: "SourceSans3-Regular" }}
+                  className="text-neutral-normal mb-2"
+                >
+                  City
+                </Text>
+                <Text
+                  style={{ fontFamily: "SourceSans3-SemiBold" }}
+                  className="text-neutral-dark-active"
+                >
+                  New York
+                </Text>
+              </View>
+
+              {/* state */}
+              <View>
+                <Text
+                  style={{ fontFamily: "SourceSans3-Regular" }}
+                  className="text-neutral-normal mb-2"
+                >
+                  State
+                </Text>
+                <Text
+                  style={{ fontFamily: "SourceSans3-SemiBold" }}
+                  className="text-neutral-dark-active"
+                >
+                  NY
+                </Text>
+              </View>
+
+              {/* zip code */}
+              <View>
+                <Text
+                  style={{ fontFamily: "SourceSans3-Regular" }}
+                  className="text-neutral-normal mb-2"
+                >
+                  ZIP Code
+                </Text>
+                <Text
+                  style={{ fontFamily: "SourceSans3-SemiBold" }}
+                  className="text-neutral-dark-active"
+                >
+                  34356
+                </Text>
+              </View>
+            </View>
+
+            {/* edit */}
+            <ButtonSecondary
+              onPress={() => {
+                router.push(`/`);
+              }}
+              title="Edit"
+              icon={<Octicons name="pencil" size={24} color="#2E323C" />}
+            />
+          </View>
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 };
 
-export default Profile;
+export default EmployeeProfile;
